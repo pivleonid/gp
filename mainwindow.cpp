b0804344efcb203fec5c101aa60de0d130fe71a3
Leonid <pivleonid@yandex.ru>
2017-09-06 19:14:36 +0300

    Допилить

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,7 +38,7 @@ void MainWindow::openFile(){
 
   QStringList var, var1;
   // 20 секунд на чтение
-  for(int i = 2; i < 20; i++){
+  for(int i = 2; i < 10; i++){
       var << excel.sheetCellInsert(sheet, i, 2).toString();
       var << excel.sheetCellInsert(sheet, i, 3).toString();
       var << excel.sheetCellInsert(sheet, i, 4).toString();
@@ -102,13 +102,91 @@ void MainWindow::openFile(){
 var.clear();
 prem.clear();
 
-//foreach (auto w, varList) {
-//    qDebug() << w;
-//    foreach (auto k, w) {
-//        qDebug() << k;
-//      }
+bool parity = false; //четность
+QString pasteStr;
+bool flag_a = false;
+bool flag_c = false;
+bool flag_d = false;
+bool flag_hl= false;
+bool flag_l = false;
+bool flag_r = false;
+bool flag_s = false;
+bool flag_vd = false;
+bool flag_vt = false;
+bool flag_x = false;
+bool flag_z = false;
+bool flag_zq = false;
+// k == 2 элементам: 1) RefDez 2) мутотня
+QList<QStringList>::iterator w;
+for ( w = varList.begin();w < varList.end(); w++) {
+    foreach (auto k, *w) {
+        if(parity == false){
+            //конденсаторы, резисторы и т.д.
+            if(k[0] == 'A' && flag_a == false){
+              pasteStr = "Устройства";
+              flag_a = true;
+              }
+            if(k[0] == 'C' && flag_c == false){
+              pasteStr = "Конденсаторы";
+              flag_c = true;
+              }
+            if(k[0] == 'D' && flag_d == false){
+              pasteStr = "Схемы интегральные";
+              flag_d = true;
+              }
+            if(k[0] == 'H' && k[1] == 'L'&& flag_hl == false){
+              pasteStr = "Светодиоды";
+              flag_hl = true;
+              }
+            if(k[0] == 'L'&& flag_l == false){
+              pasteStr = "Катушки индуктивности";
+              flag_l = true;
+              }
+            if(k[0] == 'R'&& flag_r == false){
+              pasteStr = "Резисторы";
+              flag_r = true;
+              }
+            if(k[0] == 'S'&& flag_s == false){
+              pasteStr = "Коммутация";
+              flag_s = true;
+              }
+            if(k[0] == 'V' && k[1] == 'D'&& flag_vd == false){
+              pasteStr = "Диоды";
+              flag_vd = true;
+              }
+            if(k[0] == 'V' && k[1] == 'T'&& flag_vt == false){
+              pasteStr = "Транзисторы";
+              flag_vt = true;
+              }
+            if(k[0] == 'X'&& flag_x== false){
+              pasteStr = "Контактные соединения";
+              flag_x = true;
+              }
+            if(k[0] == 'Z'&& flag_z == false){
+              pasteStr = "Фильтры";
+              flag_z = true;
+              }
+            if(k[0] == 'Z' && k[1] == 'Q'&& flag_zq == false){
+              pasteStr = "Кварцевый резонатор";
+              flag_zq = true;
+              }
 
-//  }
+          }
+        if(parity == true){
+            //строка не пуста
+            if(pasteStr.isEmpty() == false){
+                var << "" << pasteStr;
+
+                w = varList.insert(w ,var);
+                pasteStr.clear();
+                var.clear();
+              }
+
+          }
+        parity = !parity;
+      }
+
+  }
 
 
 ActiveWord word;
